e2e7325a3cc222befcf676570541c33daedc6a23
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-25 20:46:17 +0100

    src/App.jsx doesn't contain: ["Router"]

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import RecipeList from './components/RecipeList';
+// src/App.jsx
 import { Routes, Route, Link } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">Home</Link>  {/* Link to home */}
       </nav>
+      
+      {/* Define the routes for different views */}
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <AddRecipeForm />
-              <RecipeList />
+              <AddRecipeForm />   {/* Form to add a new recipe */}
+              <RecipeList />      {/* List of recipes */}
             </>
           }
         />
-        
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* Recipe details view */}
       </Routes>
     </div>
   );
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,13 +1,13 @@
 // src/main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // ✅ Make sure this line exists
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
 import App from './App';
-import './index.css'; // optional if you have styles
+import './index.css'; // Optional styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> {/* Wrap the App with BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
