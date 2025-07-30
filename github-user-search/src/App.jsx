c44d19f7af6e844e13084d511a307132fe3f2991
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-30 23:52:49 +0100

    Web Analytics

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Analytics } from '@vercel/analytics/next';
 
 import Search from './components/Search';
 
@@ -14,6 +15,7 @@ const App = () => {
       <main>
         <Search />
       </main>
+      <Analytics />
     </div>
   );
 };
