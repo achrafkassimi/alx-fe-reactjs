0a8066296a9ff2cbbbe56806ce70eda4c3d3c170
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-14 19:29:29 +0100

    1. Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage';
+
 
 // src/App.jsx
 function App() {
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold text-blue-500">Recipe Sharing Platform</h1>
+    <div className="min-h-screen bg-gray-50">
+      <HomePage />
     </div>
   );
 }
